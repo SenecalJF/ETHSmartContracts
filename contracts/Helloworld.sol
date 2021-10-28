6b80c413f8c6aa2fb97909e176421b6a860e9520
Dominic Hains <contact.dominichains@gmail.com>
2021-10-28 00:55:56 -0400

    feat: hello world contract

--- contracts/Helloworld.sol
+++ contracts/Helloworld.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity 0.8.9;
 
 contract Helloworld {
-  function hello() public pure returns (string memory) {
-    return 'Hello world!';
+  string public message;
+  address owner;
+
+  constructor(string memory _message) {
+    message = _message;
+    owner = msg.sender;
+  }
+
+  function hello() public view returns (string memory) {
+    return message;
+  }
+
+  function setMessage(string memory _message) public payable {
+    require(msg.sender == owner);
+    message = _message;
   }
 }
