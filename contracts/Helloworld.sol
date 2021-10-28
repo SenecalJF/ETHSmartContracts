3d854eeb92287f1ca716e0178b9ab1d1904352bd
Dominic Hains <contact.dominichains@gmail.com>
2021-10-27 23:08:21 -0400

    chore: prettier

---
+++ contracts/Helloworld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Helloworld {
+  function hello() public pure returns (string memory) {
+    return 'Hello world!';
+  }
+}
