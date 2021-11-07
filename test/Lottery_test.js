67e138793af47b34242c2b570a1bb4e630f9857f
Jean-François Senécal <jeanjeansenecal@gmail.com>
2021-11-07 00:00:10 -0400

    feat(Lottery): finished contract

---
+++ test/Lottery_test.js
@@ -0,0 +1,24 @@
+const Lottery = artifacts.require('Lottery');
+
+contract('Lottery', (accounts) => {
+  it('constructor should set the amount', async () => {
+    let instance = await Lottery.deployed();
+    let amount = await instance.amount();
+    assert.equal(amount, 5000);
+  });
+
+  it('should set owner to the owner address', async () => {
+    let instance = await Lottery.deployed();
+    let owner = await instance.owner();
+
+    assert.equal(owner, '0x627306090abaB3A6e1400e9345bC60c78a8BEf57');
+  });
+
+  it('should register a player', async () => {
+    let instance = await Lottery.deployed();
+    await instance.registerPlayer({ value: 5000, from: accounts[3] });
+    lotteryPlayer = await instance.getPlayers();
+    nb_entry = await instance.getNbOfentries();
+    assert.equal(lotteryPlayer[nb_entry - 1], accounts[3]);
+  });
+});
